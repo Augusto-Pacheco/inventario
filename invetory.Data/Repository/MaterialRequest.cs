0242c4935147edf3e0b3012efc6381d81e187d4d
augusto-pacheco <augsutop407@outlook.com>
2022-11-23 20:27:25 -0600

    nuevo repo

---
+++ invetory.Data/Repository/MaterialRequest.cs
@@ -0,0 +1,32 @@
+﻿using inventory.Data.Repository.Abstract;
+
+namespace inventory.Data.Repository
+{
+    internal class MaterialRequest : IMaterialRequest
+    {
+        public Task<bool> CheckIfExistsAsync(int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateAsync(int ProductId, int qty, int providerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> GetByIdAsync(int ProductId, string? select)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(int ProductId, int qty, int providerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
